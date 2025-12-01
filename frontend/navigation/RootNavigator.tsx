694651d148104da0535f090023100525b61d7d1a
Ayush Sharma <sharayush1441@gmail.com>
2025-12-01 09:09:56 +0530

    added signIn and SignUp screen

--- frontend/navigation/RootNavigator.tsx
+++ frontend/navigation/RootNavigator.tsx
@@ -5,15 +5,28 @@ import TabNavigator from './TabNavigator';
 import AddOutfitScreen from '../screens/AddOutfitScreen';
 import DesignRoomScreen from '../screens/DesignRoomScreen';
 import NewOutfitScreen from '../screens/NewOutfitScreen';
+import SignInScreen from '../screens/SignInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
 
 const RootNavigator = () => {
   const Stack = createNativeStackNavigator();
+  const isAuthenticated = false; // Replace with actual authentication logic
   return (
-    <Stack.Navigator screenOptions={{headerShown:false}}> 
-      <Stack.Screen name="Tabs" component={TabNavigator} />
-      <Stack.Screen name="AddOutfit" component={AddOutfitScreen} />
-      <Stack.Screen name="DesignRoom" component={DesignRoomScreen} />
-      <Stack.Screen name="NewOutfit" component={NewOutfitScreen} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {isAuthenticated ? (
+        <>
+          <Stack.Screen name="Tabs" component={TabNavigator} />
+          <Stack.Screen name="AddOutfit" component={AddOutfitScreen} />
+          <Stack.Screen name="DesignRoom" component={DesignRoomScreen} />
+          <Stack.Screen name="NewOutfit" component={NewOutfitScreen} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+        </>
+      )}
+
     </Stack.Navigator>
   )
 }
