c48e9c72fa0f4e912f39f42ff30cf24a1704a5e2
Ayush Sharma <sharayush1441@gmail.com>
2025-12-01 09:09:31 +0530

    sign up ui

---
+++ frontend/screens/SignUpScreen.tsx
@@ -0,0 +1,85 @@
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+
+const SignUpScreen = () => {
+    const navigation = useNavigation();
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [gender, setGender] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    const [profileImage, setProfileImage] = React.useState('');
+  return (
+    <SafeAreaView className='flex-1 bg-white'>
+                <KeyboardAvoidingView 
+                    
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'} 
+                    className="flex-1"
+                >
+                    <ScrollView 
+                        contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }} 
+                        keyboardShouldPersistTaps="handled" // Ensures button taps register while keyboard is open
+                        className="p-4"
+                    >
+                        <Text className='text-2xl font-bold text-center mb-6'>SignUp</Text>
+                        
+                        <TextInput
+                            className='border border-gray-300 p-3 mb-4 rounded-lg'
+                            value={email}
+                            onChangeText={setEmail}
+                            placeholder="Email"
+                            autoCapitalize="none"
+                            keyboardType="email-address"
+                        />
+                        <TextInput
+                            className='border border-gray-300 p-3 mb-4 rounded-lg'
+                            value={username}
+                            onChangeText={setUsername}
+                            placeholder="Username"
+                            
+                        />
+    
+                        <TextInput
+                            className='border border-gray-300 p-3 mb-4 rounded-lg'
+                            value={password}
+                            onChangeText={setPassword}
+                            placeholder="Password"
+                            secureTextEntry
+                        />
+
+                        <TextInput
+                            className='border border-gray-300 p-3 mb-4 rounded-lg'
+                            value={gender}
+                            onChangeText={setGender}
+                            placeholder="Gender"
+                            
+                        />
+                        <TextInput
+                            className='border border-gray-300 p-3 mb-4 rounded-lg'
+                            value={profileImage}
+                            onChangeText={setProfileImage}
+                            placeholder="Set Image URL (optional)"
+                            
+                        />
+    
+                        <TouchableOpacity className='bg-blue-500 p-3 rounded-lg mb-4'>
+                            <Text className='text-white text-center text-lg'>Sign Up</Text>
+                        </TouchableOpacity>
+    
+                        <TouchableOpacity 
+                        //@ts-ignore
+                        onPress={() => navigation.goBack()}>
+                            <Text className='text-blue-500 text-center text-lg'>
+                                    Already have an account? Sign In
+                            </Text>
+                        </TouchableOpacity>
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            </SafeAreaView>
+  )
+}
+
+export default SignUpScreen
+
+const styles = StyleSheet.create({})
