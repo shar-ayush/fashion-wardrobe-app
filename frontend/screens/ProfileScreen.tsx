dc25281c66ea4a4ee162d6d81968dcfaedb9859f
Ayush Sharma <sharayush1441@gmail.com>
2025-12-03 10:33:26 +0530

    display clothes added

--- frontend/screens/ProfileScreen.tsx
+++ frontend/screens/ProfileScreen.tsx
@@ -1,8 +1,12 @@
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import useAuthStore from '../store/authStore';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
+import { mpants, mshirts, pants, shoes, skirts, tops } from '../images';
+const API_BASE_URL = process.env.API_BASE_URL;
+
 
 const ProfileScreen = () => {
   const [activeTab, setActiveTab] = useState("Clothes");
@@ -15,6 +19,49 @@ const ProfileScreen = () => {
   const followerCount = user?.followerCount || 0;
   const followingCount = user?.followingCount || 0;
   const profileImage = user?.profileImage || "https://picsum.photos/100/100";
+
+  const popularClothes = [
+    ...pants,
+    ...tops,
+    ...skirts,
+    ...mpants,
+    ...mshirts,
+    ...shoes
+  ]
+
+  useEffect(() => {
+    const fetchOutfits = async () => {
+      if (!user._id || !token) return;
+      setLoading(true);
+      try {
+        const response = await axios.get(`${API_BASE_URL}/save-otfit/user/${user._id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        setOutfits(response.data);
+
+      } catch (error) {
+        console.log("Error fetching outfits:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchOutfits();
+  }, [user._id, token]);
+
+  const filteredClothes = activeCategory === "All" ? popularClothes : popularClothes.filter((item) => {
+    switch (activeCategory) {
+      case "Tops":
+        return item.type === "shirt";
+      case "Bottoms":
+        return item.type === "pants" || item.type === "skirts";
+      case "Shoes":
+        return item.type === "shoes";
+      default:
+        return true;
+    }
+  })
 
 
   return (
@@ -64,7 +111,7 @@ const ProfileScreen = () => {
           {["Clothes", "Outfits", "Collections"].map((tab) => (
             <TouchableOpacity key={tab} onPress={() => setActiveTab(tab)} className='pb-2'>
               <Text className={`text-base font-medium ${activeTab === tab ? "text-black" : "text-gray-500"}`}>{tab}</Text>
-              {activeTab === tab && <View className='h-0.5 bg-black mt-2'/>}
+              {activeTab === tab && <View className='h-0.5 bg-black mt-2' />}
             </TouchableOpacity>
           ))}
         </View>
@@ -73,10 +120,61 @@ const ProfileScreen = () => {
           {["All", "Tops", "Bottoms", "Shoes", "Outwear"].map((category) => (
             <TouchableOpacity key={category} onPress={() => setActiveCategory(category)} className={`px-3 mr-4 rounded-full ${activeCategory === category ? "text-black" : "text-gray-500"}`}>
               <Text className={`text-base font-medium ${activeCategory === category ? "text-black" : "text-gray-500"}`}>{category}</Text>
-              {activeCategory === category && <View className='h-0.5 bg-black mt-2'/>}
+              {activeCategory === category && <View className='h-0.5 bg-black mt-2' />}
             </TouchableOpacity>
           ))}
         </ScrollView>
+
+        {activeTab === "Clothes" && (
+          <View className='px-4'>
+            {filteredClothes.length == 0 ? (
+              <Text>No clothes available in this category</Text>
+            ) : (
+              <View className='flex-row flex-wrap'>
+                {filteredClothes.map((item) => (
+                  <View className='w-1/3 p-1.5'>
+                    <View
+                      style={{
+                        shadowColor: "#000",
+                        shadowOffset: { width: 0, height: 2 },
+                        shadowOpacity: 0.1,
+                        shadowRadius: 4,
+                        elevation: 3,
+                      }}
+                      className='bg-white rounded-lg shadow-sm border border-gray-100 overflow-hidden'
+                    >
+                      <Image className='w-full h-32' source={{ uri: item?.image }} resizeMode='contain' />
+                      <View className='p-2'>
+                        <Text className='text-xs font-medium text-gray-600 capitalize'>{item?.type} ({item?.gender})</Text>
+                      </View>
+                    </View>
+                  </View>
+                ))}
+              </View>
+            )}
+          </View>
+        )}
+
+        {activeTab === "Outfits" && (
+          <View>
+            {loading ? (
+              <ActivityIndicator size={"large"} color="#000" />
+            ) : outfits.length === 0 ? (
+              <Text>No Outfits saved </Text>
+            ) : (
+              <View>
+                {outfits?.map((outfit) => (
+                  <View>
+                    <View>
+                        
+                    </View>
+                  </View>
+                ))}
+                
+              </View>
+            )}
+          </View>
+        )}
       </ScrollView>
     </SafeAreaView>
   )
