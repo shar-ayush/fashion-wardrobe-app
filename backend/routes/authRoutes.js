465c56fea4f6eb7ca537f2f0c3ce06e635230758
Ayush Sharma <sharayush1441@gmail.com>
2025-12-05 15:18:14 +0530

    fixed error throwing

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -40,6 +40,8 @@ router.post("/register", async (req, res) => {
 router.post("/login", async(req,res) => {
     try {
         const {email, password} = req.body;
+        if(!email || !password) return res.status(400).json({message: "Email and password are required"});
+
         const user = await User.findOne({email});
         if(!user) return res.status(401).json({message: "Invalid credentials"});
         const isMatch = await user.comparePassword(password);
