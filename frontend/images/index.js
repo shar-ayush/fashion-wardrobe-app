d95e30fabc6269a583d6dfd88f13a0585b46192d
Ayush Sharma <sharayush1441@gmail.com>
2026-01-01 22:08:50 +0530

    fixed outfit data

--- frontend/images/index.js
+++ frontend/images/index.js
@@ -1,44 +1,55 @@
 export const pants = [
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753853814/image6_erkdps.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859286/image8_s1e9g8.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859286/image7_wc3dxq.png",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283811/8d2cb041dc8f468ab0b81181d83c0070_qcykx6.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283810/95bcf8c1cb7806004b39b6eb1616b09e_npsy3y.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283810/12b6a09c05024139e81b5059ca71d374_qgp48l.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283810/f3ee0e10c59451473fc800efd529092c_irjsnh.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283810/c3c3fbb3a8ec41a473cf2828b355a89e_hcariv.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283809/5455d316c88fddbcc228e44741386d57_t0f0on.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283809/52776a5628f9e6e65355400b65d9f0bb_tz0tx1.jpg",
   ].map((img, idx) => ({ id: idx + 1, image: img, type: "pants", gender: "f" }));
   
   export const tops = [
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859286/top1_lb1wb4.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859286/top2_lti11g.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859286/top5_kg0u6k.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859287/top4_xtudow.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859287/top7_fvxx5u.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859288/top9_sxatmr.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859287/top8_de4sz3.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859288/top6_tfdc0u.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753977657/Untitled_design_16_ppxbya.png"
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283553/f79c29ae4661592c54816b78352d4ac5_mjikc4.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283551/64a687c10de7f83e22533cc75c355523_nxhjsv.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283550/cbd4585cdfbed9974a7530b1d5d53c77_tkcbke.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283550/2b4496d8583df09330cf1a96cf029c0a_nihw0b.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283550/4c79d3c75dfbde68827bb4f33e5bac5a_xw9zvf.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767283549/03d8f02863acb6498082a4781348bd6d_bjmbjj.jpg",
   ].map((img, idx) => ({ id: idx + 1, image: img, type: "shirt", gender: "f" }));
   
   export const skirts = [
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859289/skirt3_oanqxj.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859290/skirt2_kdsmmj.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753859290/skirt1_wog7t9.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975648/Untitled_design_7_zres8n.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975914/Untitled_design_14_r1gzzf.png",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767282998/736ba648cef1be5d3d269a3a29c33f6d_yaj7hr.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767282998/c771c386a2ff099f1be163a42deb30a0_zwibxm.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767282997/29b5298028f3ca3b26b782c1fe7620c9_mnfscn.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767282996/17dda6510b963d3a08a4f922e0fd9ef5_kliaiy.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767282995/ca785c2bf3445234138bfe6bd60b9d30_dwjjaq.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767282995/8b2a8ae87682000b4f18f291698cf867_wj9w94.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767282994/bf72bda00d55920e3c46790d25af1e6b_edxl5j.jpg",
   ].map((img, idx) => ({ id: idx + 1, image: img, type: "skirts", gender: "f" }));
   
   export const mpants = [
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975629/Untitled_design_3_syip4x.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975630/Untitled_design_6_mt7zou.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975765/Untitled_design_10_bq0etd.png",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284047/1c4ca7944f13ecf8cd3adc1403945508_eno5ex.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284047/e9d357d16a57bc5a6dd35662a588d6c8_b1iovn.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284047/4d233af48f92ecacd58d28a3ce467943_eursr8.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284046/337850c0dfc16c5b501ec3cb7ae148b1_wpsqpz.jpg",
   ].map((img, idx) => ({ id: idx + 1, image: img, type: "pants", gender: "m" }));
   
   export const mshirts = [
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975660/Untitled_design_8_vmlkmo.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975660/Untitled_design_9_pfvmv4.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975885/Untitled_design_13_mfmdxd.png",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284162/c825f6349acce996cd8c016ee02fe327_rpqoi8.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284161/d410a4137f357310685fef6b702b0362_lnjsmk.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284161/aa90cff663d02ecb264b6b4dca235c64_s3pvxu.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284160/1edc9a12ab1c934d355c6ffa8a2ef9f5_ox0n9v.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284160/e2a47f83c6ea3df45f2b9343c24ea89c_ojwpoq.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284160/b1b47f33ed75383583951f65afa9d151_fuu7ud.jpg",
   ].map((img, idx) => ({ id: idx + 1, image: img, type: "shirt", gender: "m" }));
   
   export const shoes = [
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975802/Untitled_design_11_p7t2us.png",
-    "https://res.cloudinary.com/db1ccefar/image/upload/v1753975973/Untitled_design_15_kyl9nd.png",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284352/714fa1434d9f007388ddf0da7be76873_pitxbq.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284351/5977af07b36c22985c40b15c06d243b0_vt7xrz.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284351/6fcdfc09651455d3c77a2e1285239eb9_yetwvu.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284350/de61e33fdd7a6e7f2c7bdef1f8ba9f20_fpq2gt.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284350/5a54ae52d176abd34775591a38ddcae4_k20gji.jpg",
+    "https://res.cloudinary.com/dqu1bwidx/image/upload/e_background_removal/f_png/v1767284349/499ea1c90387c70d41ef257772fa1b7f_yr9dlw.jpg",
   ].map((img, idx) => ({ id: idx + 1, image: img, type: "shoes", gender: "unisex" }));
 
 
