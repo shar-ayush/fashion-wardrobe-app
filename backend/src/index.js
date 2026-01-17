d883cc849348d2c17f3586bfb8a51c080ea14ef3
Ayush Sharma <sharayush1441@gmail.com>
2026-01-17 23:38:47 +0530

    added virtual try on route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import {connectDB} from './lib/db.js'
 import cors from 'cors';
 import 'dotenv/config'
+import path from 'path';
 
 
 import authRoutes from '../routes/authRoutes.js'
 import savedOutfitsRoutes from '../routes/savedOutfitsRoute.js';
 import smartSearchRoutes from '../routes/smartSearchRoutes.js';
 import uploadToClosetRoute from '../routes/uploadToClosetRoute.js';
+import tryOnRoutes from '../routes/tryOnRoutes.js';
 
 import {seedOutfitdata} from "../src/lib/seedOutfitData.js"
 
@@ -23,11 +25,15 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve generated public files (tryon results)
+app.use('/public', express.static(path.join(process.cwd(), 'public')));
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/save-outfit", savedOutfitsRoutes);
 app.use('/api/smart-search', smartSearchRoutes);
 app.use('/api/upload-to-closet', uploadToClosetRoute);
+app.use("/api/try-on", tryOnRoutes);
 
 
 
