53cf3405f5d246e06357fc07615a591a8a48bd57
Ayush Sharma <sharayush1441@gmail.com>
2026-03-28 20:33:40 +0530

    refactor App component for improved layout structure

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,27 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigation/RootNavigator';
-import TabNavigator from './navigation/TabNavigator';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <GestureHandlerRootView>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
-
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
